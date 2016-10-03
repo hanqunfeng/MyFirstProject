b3b3a1c2465a703b8a2fb2680fb4ac360cd877c9
hanqf <hanqf2008@163.com>
2016-10-03 16:07:35 +0800

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,34 @@
+package com.company;
+
+
+public class Main {
+    public static void main(String[] args) { // write your code here
+        System.out.println(123);
+        System.out.println(456);
+        System.out.println(456);
+        System.out.println(456);
+        System.out.println(456);
+        System.out.println(456);
+        System.out.println(456);
+        System.out.println(456);
+        System.out.println(456);
+
+        System.out.println(123);
+        System.out.println(789);
+        System.out.println(789);
+        System.out.println(789);
+        System.out.println(789);
+        System.out.println(789);
+        System.out.println(789);
+        System.out.println(123);
+
+    }
+/* 你好啊 */
+
+    private String getString(String name) {
+
+        System.out.println();
+
+        return name;
+    }
+}
---
+++ src/com/demo/Demo.java
@@ -0,0 +1,21 @@
+package com.demo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by hanqunfeng on 2016/10/3.
+ */
+public class Demo {
+
+    public static void main(String[] args) {
+//        System.out.println("args = [" + args + "]");
+        List<String> list = new ArrayList<String>();
+        list.add("hello");
+        list.add("nihao");
+        for (String s : list) {
+            System.out.println("s = " + s);
+
+        }
+    }
+}
